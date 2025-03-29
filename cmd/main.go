32d0cce33f0858d21877e82ee5d36ef7180ce055
Nenad <nbursa@gmail.com>
2025-03-29 21:42:56 +0100

    feat: enable cors

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/nbursa/user-admin-api/config"
 	"github.com/nbursa/user-admin-api/routes"
@@ -13,6 +16,15 @@ func main() {
 
 	config.ConnectMongo()
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"POST", "GET", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	routes.RegisterRoutes(r)
 
